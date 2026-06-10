2a6eae72d57f0964585ebd01b7f7ce7aab6f7d8d
Karthick Raja S. <179904867+Karty1127@users.noreply.github.com>
2026-06-10 17:29:13 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal, VERSION } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Data } from './core/services/data/data';
+import { MatTableModule } from '@angular/material/table';
+
+export interface Exercise {
+  name: string;
+  sets: number;
+  reps: string;
+  rest: string;
+  type: 'Compound' | 'Isolation';
+}
+
+export interface DayPlan {
+  day: string;
+  label: string;
+  focus: string;
+  exercises: Exercise[];
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTabsModule, MatTableModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Gym-and-diet-chart');
+  public GymData: any
+  public displayedColumns: string[] = ['exercise', 'sets', 'reps', 'rest', 'type'];
+
+  constructor(private data: Data) { }
+
+  ngOnInit() {
+    console.log('Angular version:', VERSION.full);
+    this.GymData = this.data.gymPlanner.days;
+    console.log(this.GymData)
+  }
 }
